047bb46b251ba9d421d3d3e7390e08b7a3070f33
david peter paul marques <57423407+davidmarques164@users.noreply.github.com>
2025-05-24 23:53:10 -0300

    versao final mppt P&O

--- include/MPPTManager.h
+++ include/MPPTManager.h
@@ -5,33 +5,25 @@
 #include "INA219Manager.h"
 
 class MPPTManager {
-private:
-    // Variáveis de estado
-    float prevPower = 0.0000;
-    float prevVoltage = 0.00;
-    float prevCurrent = 0.00;
-    float dutyCycle = 0.20;
-    float step = 0.01;
-    const float maxVoltageOut = 25.5f;  // Limite de tensão de saída
-    
-    // Variáveis para filtros
-    float filteredVoltageIn = 0.00;
-    float filteredCurrentIn = 0.00;
-    float filteredPowerIn = 0.00;
-    
-    // Parâmetros dos filtros
-    const float voltageFilterAlpha = 0.9f;
-    const float currentFilterAlpha = 0.9f;
-    
-    // Parâmetros do passo adaptativo
-    const float minStep = 0.005f;
-    const float maxStep = 0.03f;
-    const float powerThresholdFar = 5.0f;  // Limiar para passo grande
-    const float powerThresholdNear = 1.0f; // Limiar para passo médio
-
 public:
     void begin();
     float update(INA219Manager& ina);
+
+private:
+    float prevPower = 0.0f;
+    float prevVoltage = 0.0f;
+    float prevCurrent = 0.0f;
+    float dutyCycle = 0.2f;
+    float filteredVoltageIn = 0.0f;
+    float filteredCurrentIn = 0.0f;
+    float filteredPowerIn = 0.0f;
+    float vMPPT = 0.0f;
+
+    const float voltageFilterAlpha = 0.1f;
+    const float currentFilterAlpha = 0.1f;
+    const float step = 0.2f;
+    const float maxVoltageOut = 25.5f;
+
     void setPWM(float pwm);
 };
 
